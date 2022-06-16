706c0a3b81d4cf5f09373414b553268a0818490b
juanpmf13 <juanpablomonteirofernandes@id.uff.br>
2022-06-16 20:03:27 -0300

    kotlin introdução 1.1 private e seus usos

--- introducao_OO_Kotlin/src/main/kotlin/Introducao_OO/Pessoa.kt
+++ introducao_OO_Kotlin/src/main/kotlin/Introducao_OO/Pessoa.kt
@@ -3,18 +3,24 @@ package Introducao_OO
 class Pessoa {
     var nome: String = "juan"
     var cpf: String = "123.456.789-10"
+
+    private set
+
     inner class Endereco{
         var rua:String="são pedro"
 
+    }
+
+    fun getcpf():String{
+        return cpf
     }
 
 }
 
 fun main(){
     val jether = Pessoa()
-
     println(jether.nome)
-    println(jether.cpf)
+    println(jether.getcpf())
     println(jether.Endereco().rua)
 
 }
