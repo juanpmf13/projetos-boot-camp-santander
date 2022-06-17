7250be9000f03ddfafb40b274efc73cd1d4b5ddb
juanpmf13 <juanpablomonteirofernandes@id.uff.br>
2022-06-16 23:31:05 -0300

    kotlin introdução 1.3 data class

---
+++ introducao_OO_Kotlin/src/main/kotlin/Introducao_OO/Banco.kt
@@ -0,0 +1,6 @@
+package Introducao_OO
+
+data class Banco(
+    val nome: String,
+    val numero: Int
+)
---
+++ introducao_OO_Kotlin/src/main/kotlin/Introducao_OO/Teste/BancoTeste.kt
@@ -0,0 +1,11 @@
+package Introducao_OO.Teste
+
+import Introducao_OO.Banco
+
+fun main(){
+    val digiOneBank = Banco("DigiOne", 134)
+
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+}
